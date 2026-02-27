43f0de14f821118ff3dd2f7567b8fac16c3cce3a
saliinao <humagain.salina123@gmail.com>
2026-02-27 21:22:54 +0545

    used dma to allocate memory

--- pointer.c
+++ pointer.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
+    int* ptr_num = (int*)malloc(sizeof(int));
+    float* ptr_decimal = (float*)malloc(sizeof(float));
+
+    *ptr_num = 6;
+    *ptr_decimal= 6.4f; // not mandatory but good practice to write f
+
     printf("hello world");
+
     return 0;
 }
