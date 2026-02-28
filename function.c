aa1ade9860dd4c3c736b91cd928e33a488e9637d
saliinao <humagain.salina123@gmail.com>
2026-02-28 22:27:39 +0545

    refactor: fixed formatting

--- function.c
+++ function.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-int add(int a,int b) {
-    return a+b;
+int add(int a, int b) {
+    return a + b;
 }
+
 int main() {
-    int sum = add(4,5);
+    int sum = add(4, 5);
+
     printf("%d", sum);
+
     return 0;
 }
