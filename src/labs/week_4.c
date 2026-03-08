2cb28c59d9e78eb6b9f521eb302158d8d810cd89
saliinao <humagain.salina123@gmail.com>
2026-03-08 23:17:37 +0545

    feat: complete question 1

---
+++ src/labs/week_4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // Write a program that reads a number and identifies whether the given number is even or odd.
+    int number;
+
+    printf("Enter a number:");
+    scanf("%d", &number);
+
+    if (number % 2 == 0) {
+        printf("Even");
+    }
+    else {
+        printf("Odd");
+    }
+
+
+    // 2. Write a program to find the largest number among three numbers.
+}
