39cf6ad6c9671366ca1e601e2b532fa97eef0770
saliinao <humagain.salina123@gmail.com>
2026-03-08 21:40:29 +0545

    feet: complete week 2

---
+++ src/labs/week_2.c
@@ -0,0 +1,17 @@
+//Some sample C program.
+#include <stdio.h>
+
+int main (){
+    printf("hello world");
+
+    // Some problems related to formatted strings.
+    
+    printf("backslash: \\");
+    printf("\n");
+
+    printf("New Line: \\n");
+    printf("\n");
+
+    printf("Tab character: \\t");
+    printf("\n");
+}
